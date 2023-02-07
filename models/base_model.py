d6079dd0552ba4070662a2d4a920854c0dd5da9e
vickouma77 <vickprestone20@gmail.com>
2023-02-07 04:49:15 -0800

    adding BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines the BaseModel class"""
+
+import datetime
+import uuid
+import string
+
+
+class BaseModel:
+    """BaseModel for the project"""
+
+    def __init__(self):
