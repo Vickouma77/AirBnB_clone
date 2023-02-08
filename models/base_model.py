f6d2a27cd437a0649cc567b8734abd76c044d8fb
vickouma77 <vickprestone20@gmail.com>
2023-02-08 02:54:45 -0800

    defining BaseModel method

--- models/base_model.py
+++ models/base_model.py
@@ -9,4 +9,10 @@ import string
 class BaseModel:
     """BaseModel for the project"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel.
+
+        Args:
+            *args: not used.
+            **kwargs(dict): key/value pairs of attributes.
+        """
