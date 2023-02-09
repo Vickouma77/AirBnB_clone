a2ce2d6e532296bd1329f995a7e6b510fef3624b
vickouma77 <vickprestone20@gmail.com>
2023-02-09 04:41:42 -0800

    adding public instance methods

--- models/base_model.py
+++ models/base_model.py
@@ -25,3 +25,10 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
 
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
