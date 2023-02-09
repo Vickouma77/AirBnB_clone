0865e6c6559969807417ee07f11b7caa1a5ec530
vickouma77 <vickprestone20@gmail.com>
2023-02-09 12:34:00 -0800

    adding BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -37,3 +37,8 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all keys/values
            of __dict__ of the instance"""
+           new_dict = self.__dict__.copy()
+           new_dict["created_at"] = self.created_at.isoformat()
+           new_dict["updated_at"] = self.updated_at.isoformat()
+           new_dict["__class__"] = self.__class__name
+           return new_dict
