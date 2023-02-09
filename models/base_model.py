348ed7d279d853b88a63d18cfd3adff4137c7169
vickouma77 <vickprestone20@gmail.com>
2023-02-09 12:15:35 -0800

    updating BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -19,19 +19,21 @@ class BaseModel:
         form_t = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
-        self.updated_at = datetime.todat()
+        self.updated_at = datetime.today()
 
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                     self.__dict__[key] = datetime.strptime(value, form_t)
-                 else:
-                     self.__dict__[key] = value
+                    self.__dict__[key] = datetime.strptime(value, form_t)
+                else:
+                    self.__dict__[key] = value
 
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """updates the public instance attribute updated_at
+           with the current datetime"""
         self.updated_at = datetime.today()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """returns a dictionary containing all keys/values
+           of __dict__ of the instance"""
