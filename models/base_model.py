9d5200522909ff53b4140bf68755ded6774052c1
vickouma77 <vickprestone20@gmail.com>
2023-02-10 12:36:21 -0800

    updating models

--- models/base_model.py
+++ models/base_model.py
@@ -33,6 +33,7 @@ class BaseModel:
         """updates the public instance attribute updated_at
            with the current datetime"""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
