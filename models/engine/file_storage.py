d13c72ed658d08df3544453ce50a57c6bf2dd92a
vickouma77 <vickprestone20@gmail.com>
2023-02-10 12:21:44 -0800

    updating file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,12 +3,14 @@
 import json
 from models.base_model import BaseModel
 
+
 class FileStorage:
     """Represent an abstracted storage engine.
-    Attributes:
+       Attributes:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
     """
+
     __file_path = "file.json"
     __objects = {}
 
