294a24c0988fcb0a322d55a20c8c85b3b9be8f0a
vickouma77 <vickprestone20@gmail.com>
2023-02-10 12:23:20 -0800

    updating file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,7 +29,7 @@ class FileStorage:
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, "w") as f:
             json.dump(objdict, f)
-           
+
     def reload(self):
         """deserializes the JSON file to __objects exists"""
         try:
