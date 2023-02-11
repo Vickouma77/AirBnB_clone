b737a83e4c2b765ff763826d601614e2dbd36e19
vickouma77 <vickprestone20@gmail.com>
2023-02-11 12:14:49 -0800

    updating city class

--- models/city.py
+++ models/city.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 """Defines city class"""
 
+
+class City(BaseModel):
+    """Defines the city class
+
+    Attributes:
+          state_id: string - empty string: it will be the State.id
+          name: string - empty string
+    """
+
+    state_id = ""
+    name = ""
