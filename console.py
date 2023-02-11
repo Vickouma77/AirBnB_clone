5b032bb8a3ce7c41eb3ab7e276e3f324415cac04
vickouma77 <vickprestone20@gmail.com>
2023-02-11 12:46:31 -0800

    updating console

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.state import State
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
+from models.review import Review
 
 
 def parse(arg):
