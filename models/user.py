6d7c09f6082397f616f81e4bb118ad69dd6ed60d
vickouma77 <vickprestone20@gmail.com>
2023-02-11 11:49:38 -0800

    creating user class

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the user class"""
+
+
+class User:
+    """Represents a user.
+
+    Attributes:      
+        email: string - empty string
+        password: string - empty string
+        first_name: string - empty string
+        last_name: string - empty string
+    """
