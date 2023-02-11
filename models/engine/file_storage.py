345551a3773ca432953ca2a0d179e0c2a024db05
vickouma77 <vickprestone20@gmail.com>
2023-02-11 13:06:30 -0800

    updating storage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,12 +2,12 @@
 """Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
-from models.user import user
-from models.state import state
-from models.city import city
-from models.amenity import amenity
-from models.place import place
-from models.review import review
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
