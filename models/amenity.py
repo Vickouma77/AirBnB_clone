c9b1696db847e8ea56cccd441e513359e7f1f091
vickouma77 <vickprestone20@gmail.com>
2023-02-11 12:21:55 -0800

    updating amenity class

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines the amenity class"""
+
+
+class Amenity(BaseModel):
+    """Defines the amenity class
+
+    Attributes:
+          name(str): empty
+    """
+
+    name = ""
