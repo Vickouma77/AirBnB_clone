95dedfd767fdedb5213defd66b94cfb573549256
vickouma77 <vickprestone20@gmail.com>
2023-02-11 12:45:06 -0800

    updating console

--- console.py
+++ console.py
@@ -8,6 +8,7 @@ from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
+from models.place import Place
 
 
 def parse(arg):
