321f1366f8b02a5c59adac84a4d8fe5b1fd91ecf
vickouma77 <vickprestone20@gmail.com>
2023-02-11 12:23:41 -0800

    updating storage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 from models.user import user
 from models.state import state
 from models.city import city
+from models.amenity import amenity
 
 
 class FileStorage:
