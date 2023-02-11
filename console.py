fbe86e8230bf0d92c0603a776b17047b5aa96d81
vickouma77 <vickprestone20@gmail.com>
2023-02-11 09:54:40 -0800

    updating console

--- console.py
+++ console.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-"""Defines HBnB console""
-
+"""Defines HBnB console"""
+import cmd
+import re
+from shlex import split
+from models import storage
+from models.base_model import BaseModel6
